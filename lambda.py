4eb244033793396e44bfc9c444ab416295710465
Ronniawan Fadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2026-09-19 00:07:09 +0700

    Create lambda.py
    Create lambda notes (ternary operator)

---
+++ lambda.py
@@ -0,0 +1,19 @@
+# Lambda's example
+
+import math
+
+# Ternary operator. using if else with lambda
+# using modulus to detect even or odd on number
+oddEven=lambda x:"Even" if x % 2==0 else "Odd"
+print(oddEven(4))
+
+# Nested ternary
+poin=lambda poin:("a" if poin>=85 else ("b" if poin >=70 and poin <=84 else("c" if poin >=60 and poin <=69 else "d")))
+print("Result poin : "+poin(67))
+
+# Dispatcher pattern
+# Calculate the square if the number is positive, or the absolute square root if it is negative. 0=positive
+number=lambda x: (x ** 2) if x >= 0 else math.sqrt(abs(x))
+print(number(2)) # Result 4
+print(number(0)) # Result still 0
+print(number(-2)) # result 1.4142135....
