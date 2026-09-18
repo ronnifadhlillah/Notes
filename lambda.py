3be3465a1d1f94b6e90baca6faa61c7f48a102b2
Ronniawan Fadhlillah <33772449+ronnifadhlillah@users.noreply.github.com>
2026-09-19 00:21:34 +0700

    Update lambda.py
    Ternary combination with map()

--- lambda.py
+++ lambda.py
@@ -17,3 +17,9 @@ number=lambda x: (x ** 2) if x >= 0 else math.sqrt(abs(x))
 print(number(2)) # Result 4
 print(number(0)) # Result still 0
 print(number(-2)) # result 1.4142135....
+
+# Ternary combination with map()
+# If the number is less than 0, change it to 0. If it is even, divide by 2. If it is odd, multiply by 3
+nl = [-5, 12, 0, 8, -3, 20]
+processNl = list(map(lambda x: 0 if x < 0 else (x // 2 if x % 2 == 0 else x * 3),nl))
+print(processNl) # Result [0, 6, 0, 4, 0, 10]
